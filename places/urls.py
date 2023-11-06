f931751760d7666b33038f4b3829919201a58713
Vlada <vlada97.tr@gmail.com>
2023-11-06 17:51:35 +0300

    views json

--- places/urls.py
+++ places/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("places/<int:place_id>/", views.place_detail, name="detail"),
+]
---
+++ places/views.py
@@ -0,0 +1,38 @@
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Place
+
+
+def index(request):
+    serialized_places = []
+    for place in Place.objects.all():
+        place = {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [place.lng, place.lat]},
+            "properties": {
+                "title": place.title,
+                "placeId": place.pk,
+                "detailsUrl": reverse("detail", args=(place.pk,)),
+            },
+        }
+        serialized_places.append(place)
+
+    collection = {"type": "FeatureCollection", "features": serialized_places}
+    return render(request, "index.html", context={"places": collection})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    content = {
+        "title": place.title,
+        "imgs": [image.image.url for image in place.images.order_by("position")],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lng,
+        },
+    }
+    return JsonResponse(content, json_dumps_params={"ensure_ascii": False, "indent": 2})
