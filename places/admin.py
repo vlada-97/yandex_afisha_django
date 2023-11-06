91c6f26beaafd1c9e2b07f8a4f11b63bf71f34d6
Vlada <vlada97.tr@gmail.com>
2023-11-06 19:23:29 +0300

    add photo preview

--- places/admin.py
+++ places/admin.py
@@ -5,10 +5,29 @@ from .models import Place, Image
 from django.contrib import admin
 
 from .models import Place, Image
+from django.utils.html import format_html
+
+
+def get_html_preview(image):
+    return format_html(
+        '<img src="{url}" height={height}/>',
+        url=image.image.url,
+        height=200,
+    )
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    readonly_fields = [
+        "get_preview",
+    ]
+
+    @staticmethod
+    def get_preview(obj):
+        return get_html_preview(obj)
+
+    get_preview.short_description = "Фото превью"
 
 
 @admin.register(Place)
@@ -18,10 +37,26 @@ class AdminPlace(admin.ModelAdmin):
     ]
     list_display = [
         "title",
+        "get_preview",
     ]
     inlines = [
         ImageInline,
     ]
+
+    def get_preview(self, obj):
+        return get_html_preview(obj.images.first())
+
+    get_preview.short_description = "Фото превью"
+
+    def get_preview_readonly(self, obj):
+        return self.get_preview(obj)
+
+    get_preview_readonly.short_description = "Фото превью"
+    get_preview_readonly.readonly = True
+
+    readonly_fields = ["get_preview_readonly"]
+
+    get_preview.admin_order_field = "images__image"
 
 
 @admin.register(Image)
@@ -29,11 +64,20 @@ class AdminImage(admin.ModelAdmin):
     fields = (
         "place",
         "image",
+        "get_preview",
     )
     list_display = [
         "position",
         "place",
+        "get_preview",
     ]
-    list_filter = [
-        "place",
+    list_filter = ["place"]
+    readonly_fields = [
+        "get_preview",
     ]
+
+    @staticmethod
+    def get_preview(obj):
+        return get_html_preview(obj)
+
+    get_preview.short_description = "Фото превью"
