90b73e11272a6e4c529e3a5acd7aa52d1a64131d
Vlada <vlada97.tr@gmail.com>
2023-11-07 17:16:52 +0300

    some fixes

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,12 +9,11 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG", "true")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS=["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
@@ -118,7 +117,6 @@ MEDIA_URL = "media/"
 # STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 MEDIA_ROOT = "media/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import re_path, include
-from where_to_go import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+
 
 urlpatterns = [
     re_path("admin/", admin.site.urls),
